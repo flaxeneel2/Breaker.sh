ae7c79c6a8f6a53213a5cdd50829db4f3b29e92c
flaxeneel2 <flaxeneel2@gmail.com>
2022-02-21 17:25:27 +0100

    fixed file perm issues as well as docker cache not working

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
-FROM debian:latest
+FROM debian:stable
+
+RUN apt-get update && apt-get upgrade && apt-get install toilet build-essential git curl boxes figlet -y \
+    && adduser --gecos "" --disabled-password --home /home/container container
 
 COPY ./breaker /breaker
 
-RUN apt update && apt upgrade && apt install toilet build-essential git curl boxes figlet -y \
-    && adduser --gecos "" --disabled-password --home /home/container container && chmod 0110 /breaker
+RUN chmod 0111 /breaker
 
 USER container
 ENV  USER=container HOME=/home/container TERM=xterm-256color
--- breaker.sh
+++ breaker.sh
@@ -1,2 +1,57 @@
 #!/bin/bash
-echo "hi"
+
+################################################
+#                    Colors                    #
+################################################
+
+#reset
+NC='\033[0m'
+
+#normal colors
+RED='\033[0;31m'
+BLACK='\033[0;30m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+DGRAY='\033[1;30m'
+
+#light colors
+LRED='\033[0;31m'
+LBLACK='\033[0;30m'
+LGREEN='\033[0;32m'
+LYELLOW='\033[1;33m'
+LBLUE='\033[1;34m'
+LPURPLE='\033[1;35m'
+LCYAN='\033[1;36m'
+LWHITE='\033[1;37m'
+
+#text types
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+UNDERLINE=$(tput smul)
+BLINK=$(tput blink)
+REV=$(tput rev)
+STANDOUT=$(tput smso)
+
+################################################
+#                     Main                     #
+################################################
+
+#toilet -f smblock --filter border:metal 'Breaker.sh'
+echo -e "${PURPLE}1${LPURPLE}) ${PURPLE}Minecraft java"
+echo -e "${PURPLE}2${LPURPLE}) ${PURPLE}Minecraft bedrock"
+echo -e "${PURPLE}3${LPURPLE}) ${PURPLE}Discord Bots"
+read -p "${NORMAL}Selection: " OPTION
+if [[ "$OPTION" = "1" ]]; then
+  echo -e "${PURPLE}1${LPURPLE}) ${PURPLE}PaperMC"
+  read -p "${NORMAL}Selection: " OPTION_TWO
+  if [[ "$OPTION_TWO" = "1" ]]; then
+    echo -e "${PURPLE}Installing PaperMC${DGRAY}"
+  fi
+
+else
+  echo "2"
+fi
